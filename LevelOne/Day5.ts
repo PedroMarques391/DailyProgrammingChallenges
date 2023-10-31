b33c1bdf76d9d86477fb6eeb95fe4a4b0090b9c5
Pedro <pedromarques391@gmail.com>
2023-10-31 18:47:57 -0300

    Adicioando dia 5.

---
+++ LevelOne/Day5.ts
@@ -0,0 +1,11 @@
+function isString(word: string | number | boolean): string {
+    if (typeof(word) === 'string') {
+        return "A palavra é uma string"
+    } else {
+        return `A palavra é do tipo ${typeof(word)}`
+    }
+}
+
+console.log(isString('Essa é uma palavra.'))
+console.log(isString(458))
+console.log(isString(true))
