fa7e5710b0e8434a2e4089c77e3e4d9aae31d050
Pedro <pedromarques391@gmail.com>
2023-10-31 18:10:29 -0300

    Adicionando dias 3 e 4.

---
+++ LevelOne/Day4.ts
@@ -0,0 +1,6 @@
+function average(note1: number, note2: number, note3: number): string {
+    const finalAverage: number = ((note1 + note2 + note3) / 3)
+    return `A nota final é ${finalAverage.toFixed(2)}`
+}
+
+console.log(average(3.5, 4.5, 8.0))
