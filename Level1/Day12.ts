4f22981673a2f9c353961f5302ebbabdbe5b60ab
Pedro <pedromarques391@gmail.com>
2023-11-01 15:49:57 -0300

    Finalizando dia 12.

--- Level1/Day12.ts
+++ Level1/Day12.ts
@@ -3,11 +3,9 @@ let sum = 0
 function sumPairs(num: number): string {
     for (let n = 0; n <= num; n++) {
         let num = n
-        num % 2 === 0 ? sum += num : num
-        
+        num % 2 === 0 ? sum += num : num 
     }
    return `A soma dos numeros pares até ${num} é igual a ${sum}.`
 }
-
 console.log(sumPairs(1205))
 
---
+++ Level1/Day13.ts
@@ -0,0 +1,8 @@
+let mult: string;
+function multply(num: number) {
+    for(let i = 0; i <= 10; i++) {
+        mult = `${num} * ${i} = ${num * i}`
+        console.log(mult)
+    } 
+}
+multply(8)
