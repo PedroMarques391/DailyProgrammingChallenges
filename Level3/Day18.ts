c3094f194d0a546278e3b9fb06fdc30885a6af97
Pedro <pedromarques391@gmail.com>
2023-11-01 18:45:29 -0300

    Finalizando dia 18

---
+++ Level3/Day18.ts
@@ -0,0 +1,10 @@
+let password: string = '';
+function randomPassword(): string {
+    const passWordIndices = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&*+'
+    while(password.length < 8) {
+        const letterIndice = Math.floor(Math.random() * passWordIndices.length)
+        password += passWordIndices[letterIndice]
+    }
+    return `Sua senha é ${password}`
+}
+console.log(randomPassword())
