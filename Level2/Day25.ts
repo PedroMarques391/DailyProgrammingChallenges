f96c79503f83d6011b764d466a0a00a11b106398
Pedro <pedromarques391@gmail.com>
2023-11-04 14:20:02 -0300

    Finalizando desafio do dia 25.

---
+++ Level2/Day25.ts
@@ -0,0 +1,27 @@
+interface Unotes {
+    [key: string]: string
+}
+
+const notas: Unotes = {
+    "cebola": "Dó",
+    "alho": "Ré",
+    "pimenta": "Ré",
+    "tomate": "Mi",
+    "sal": "Fa",
+    "ovo": "Sol",
+    "salsa": "Lá",
+    "azeite": "Si",
+}
+const notasArray: string[] = []
+function getNotes (arr: string[]): string {
+    arr.forEach((n: string) => {
+      if(notas[n]) {
+        notasArray.push(notas[n])
+      } else {
+        throw new Error(`Chave não encontrada: ${n}`);
+      }
+   })
+   return `A sequência de notas musicais é ${notasArray}`
+}
+
+console.log(getNotes(["alho", "cebola", "ovo"]))
