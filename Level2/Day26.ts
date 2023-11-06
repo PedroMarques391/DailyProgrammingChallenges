8c6421e6bc74b5168a1cb6228ea048b5727172f8
Pedro <pedromarques391@gmail.com>
2023-11-06 13:32:36 -0300

    Finalizando dia 26.

---
+++ Level2/Day26.ts
@@ -0,0 +1,34 @@
+let musics: string[] = [];
+let music : string[] = []
+
+function addSongs (song: string): string {
+    musics.push(song)
+    return `Música adicionada com sucesso.`
+}
+
+addSongs('Car crash')
+addSongs('Human')
+addSongs('snuff')
+
+console.log(musics)
+function removeSongs (song: string) {
+    music = musics.filter((music) => music !== song)
+    musics = music
+    
+}
+
+removeSongs('Human');
+
+function listSongs() {
+    for (let i = 0; i < musics.length; i++) {
+        console.log(`${i + 1} - ${musics[i]}`)
+    }
+}
+
+listSongs()
+
+
+
+
+
+
