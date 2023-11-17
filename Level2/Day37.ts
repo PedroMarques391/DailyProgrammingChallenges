13030c4ba0f75572775c6d8cb58ec3179f64a937
Pedro <pedromarques391@gmail.com>
2023-11-17 15:38:33 -0300

    Finalizando dia 37 e atualizando README

---
+++ Level2/Day37.ts
@@ -0,0 +1,16 @@
+const codedMessage: string = "Khoor Zruog";
+const keys: number =  3;
+
+function decrypt(coded: string, keys: number): string {
+    let decodedWord: string = '' 
+    for(let i = 0; i < codedMessage.length; i++) {
+        const ASCIInumber: number = 
+        coded.charCodeAt(i) === 32 
+        ? coded.charCodeAt(i) 
+        : coded.charCodeAt(i) - keys
+        decodedWord += String.fromCharCode(ASCIInumber);
+    }
+    return decodedWord;
+};
+
+console.log(decrypt(codedMessage, keys))
