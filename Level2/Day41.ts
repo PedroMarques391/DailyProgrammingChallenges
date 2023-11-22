ea21e629953cd5ee7bcd60c2ed9f7a91a6632d4b
Pedro <pedromarques391@gmail.com>
2023-11-22 16:44:38 -0300

    Finalizando dia 41 e atualizando README.

--- Level2/Day41.ts
+++ Level2/Day41.ts
@@ -1,18 +1,18 @@
-function encontraUnico(arr: number[]) {
-    arr.sort((a, b) => a - b);
-    for (let i = 0; i < arr.length; i+= 2) {
-        if (i + 1 >= arr.length || arr[i] !== arr[i + 1]) {
-            return arr[i];
+function uniqueNumber(numArray: number[]) {
+    numArray.sort((a, b) => a - b);
+    for (let i = 0; i < numArray.length; i+= 2) {
+        if (i + 1 >= numArray.length || numArray[i] !== numArray[i + 1]) {
+            return numArray[i];
         }
        
     }
-    return arr.join('')
+    return numArray.join('')
 }
 
-console.log(encontraUnico([1,2,3,1,2]));//Saída: 3
-console.log(encontraUnico([1,2,3,1,2, 3, 4]));//Saída: 4
-console.log(encontraUnico([1,2,3,1,2, 3, 4, 4, 5]));//Saída: 5
-console.log(encontraUnico([1,2,3,1,2, 3, 4, 4, 5, 5, 6]));//Saída: 6
-console.log(encontraUnico([1,2,3,1,2, 3, 4, 4, 5, 5, 6, 6, 7]));//Saída: 7
+console.log(uniqueNumber([1,2,3,1,2]));//Saída: 3
+console.log(uniqueNumber([1,2,3,1,2, 3, 4]));//Saída: 4
+console.log(uniqueNumber([1,2,3,1,2, 3, 4, 4, 5]));//Saída: 5
+console.log(uniqueNumber([1,2,3,1,2, 3, 4, 4, 5, 5, 6]));//Saída: 6
+console.log(uniqueNumber([1,2,3,1,2, 3, 4, 4, 5, 5, 6, 6, 7]));//Saída: 7
 
 
