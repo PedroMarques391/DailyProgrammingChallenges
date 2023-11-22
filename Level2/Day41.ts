e09bc299ae4d9b2b3ee9be163cfc92996d0a9aa3
Pedro <pedromarques391@gmail.com>
2023-11-22 16:57:54 -0300

    Finalizando dia 41 e atualizando README

--- Level2/Day41.ts
+++ Level2/Day41.ts
@@ -14,7 +14,6 @@ console.log(uniqueNumber([1, 2, 3, 1, 2, 3, 4]));//Saída: 4
 console.log(uniqueNumber([1, 2, 3, 1, 2, 3, 4, 4, 5]));//Saída: 5
 console.log(uniqueNumber([1, 2, 3, 1, 2, 3, 4, 4, 5, 5, 6]));//Saída: 6
 console.log(uniqueNumber([1, 2, 3, 1, 2, 3, 4, 4, 5, 5, 6, 6, 7]));//Saída: 7
-console.log(uniqueNumber([1, 2, 3, 1, 2, 3, 4, 4, 5, 5, 6, 6, 7]));//Saída: 7
 console.log(uniqueNumber([2, 4, 6, 16, 18, 20, 6, 8, 10, 12, 14, 8, 10, 12, 14, 16, 18, 20, 2]));//Saída: 4
 
 
